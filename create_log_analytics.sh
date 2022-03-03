fd1acf3a148fcdf050fd150729252147b8cdfe40
Idan Regev <idanre1@gmail.com>
2022-03-03 13:50:02 +0200

    hide secrets

--- create_log_analytics.sh
+++ create_log_analytics.sh
@@ -3,6 +3,9 @@
 # https://trstringer.com/systemd-journal-to-syslog-azure-monitoring/
 # cmd.sh <resource_group> <logs_workspace_name>
 
+# install on linux client:
+# wget https://raw.githubusercontent.com/Microsoft/OMS-Agent-for-Linux/master/installer/scripts/onboard_agent.sh && sh onboard_agent.sh -w $WORKSPACE_ID -s $WORKSPACE_KEY -d opinsights.azure.com
+
 RG=$1
 NAME=$2
 
@@ -11,7 +14,5 @@ echo "*** Creating logs workspace"
 
 WORKSPACE_ID=`az monitor log-analytics workspace show --resource-group $RG --workspace-name $NAME --query customerId -o tsv`
 WORKSPACE_KEY=`az monitor log-analytics workspace get-shared-keys --resource-group $RG --workspace-name $NAME --query primarySharedKey -o tsv`
-echo $WORKSPACE_ID
-echo $WORKSPACE_KEY
 
 /nas/azure-env-crypt/args_to_aes_file.sh WORKSPACE_ID=$WORKSPACE_ID WORKSPACE_KEY=$WORKSPACE_KEY
