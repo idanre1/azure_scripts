ecddcaf1bd2cf17ac29ff284ff78e3857a91f497
Idan Regev <idanre1@gmail.com>
2022-03-23 15:57:10 +0000

    install draft

---
+++ postgres/install_postgres_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Check arguments
+if [[ $# < 2 ]]; then
+	echo "Usage: $0 <json_file> <db_path>"
+	exit 1
+fi
+
+# conf file exists
+if [[ ! -e $1 ]]; then
+	echo "Error: json conf file $1 does not exists"
+	exit 1
+fi
+JSON=$1
+chmod 600 $JSON
+
+# postgres persistant path exists
+if [[ ! -d $2 ]]; then
+    echo "Error: db_path $2 does not exists"
+    exit 1
+fi
+DB_PATH=$2
+
+# ---------------------------
+# main
+# ---------------------------
+sudo mkdir $DB_PATH/database_archive_wal
+sudo chown postgres:postgres $DB_PATH/database_archive_wal
+
