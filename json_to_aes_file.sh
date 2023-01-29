e153c3099bd4cd1a494d35d92b58c62d66cb429c
Idan Regev <idanre1@gmail.com>
2023-01-29 10:42:06 +0200

    pass credentials using json

---
+++ json_to_aes_file.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Usage:
+# json_to_aes_file.sh < filename.json
+
+ARGS=`python -c "import sys; import json; j=json.loads(sys.stdin.read()); s=''
+for k,v in j.items(): s+=f'{k}={v} '
+print(s)" < $1`
+
+echo "*** Please enter filename:   "
+read name
+echo "*** Please enter password:   "
+stty -echo
+read password
+stty echo
+
+
+python $HOME/python_lib/azure_env_crypt/args_to_aes_file.py -f $name -p $password -d $ARGS
+
