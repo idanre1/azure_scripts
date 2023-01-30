b30ed16b3fb21fe7638953100b4f38230f8ea5f4
Idan Regev <idanre1@gmail.com>
2023-01-30 09:16:41 +0200

    check for missing file

--- json_to_aes_file.sh
+++ json_to_aes_file.sh
@@ -2,6 +2,13 @@
 # Usage:
 # json_to_aes_file.sh < filename.json
 
+if [ -e $1 ]; then
+    echo ""
+else
+    echo "*** Error: json does not exists"
+    exit
+fi
+
 ARGS=`python -c "import sys; import json; j=json.loads(sys.stdin.read()); s=''
 for k,v in j.items(): s+=f'{k}={v} '
 print(s)" < $1`
