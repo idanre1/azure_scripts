d85357f8f8b3eca6d0d0bfec2d727538f21600c1
Idan Regev <idanre1@gmail.com>
2023-10-17 09:14:10 +0300

    only take file one

--- events/print_only_created_blobs.py
+++ events/print_only_created_blobs.py
@@ -4,7 +4,7 @@ import re
 import argparse
 parser = argparse.ArgumentParser(description='python handler example')
 parser.add_argument('json_string', type=str, help='event result to process in json format (serialized)')
-parser.add_argument('container', type=str, default='raw', help='default container name to listen to')
+parser.add_argument('--container', type=str, default='raw', help='default container name to listen to')
 args = parser.parse_args()
 
 result = json.loads(str(args.json_string))
@@ -14,7 +14,7 @@ event_type = result['eventType']
 # https://crwar.blob.core.windows.net/raw/install_droplet.sh
 raw_filename = result['data']['blobUrl']
 comp_reg = re.compile(f"https.*?blob\.core\.windows\.net\/{args.container}\/(.*)")
-filename = comp_reg.findall(raw_filename) 
+filename = comp_reg.findall(raw_filename)[0]
 
 # Type handler
 if event_type == 'Microsoft.Storage.BlobDeleted':
